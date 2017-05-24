44ab475de4b810800f7fc5d2ca18b3899560cfd5
Stefan Zweifel <hello@stefanzweifel.io>
2017-05-24 23:04:54 +0200

    Add json-feed.php Plugin and Changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog
+
+### v1.0.0
+
+- Implement JSON Feed v1 spec
---
+++ json-feed.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+* JSON Feed Plugin
+*
+* @author Stefan Zweifel <hello@stefanzweifel.io>
+* @version 1.0.0
+*/
+Pages::$methods['jsonfeed'] = function($pages, $params = array()) {
+
+    // set all default values
+    $defaults = array(
+        'title'       => 'Feed',
+        'feed'        => url(),
+        'datefield'   => 'date',
+        'textfield'   => 'text',
+    );
+
+    // merge them with the user input
+    $options = array_merge($defaults, $params);
+
+    $items = array();
+
+    foreach($pages as $item) {
+        $items[] = array(
+            'id' => $item->url(),
+            'url' => $item->url(),
+            'title' => $item->title()->value(),
+            'content_html' => $item->{$options['textfield']}()->kirbytext()->value,
+            'date_modified' => $item->date('c', $options['datefield'])
+        );
+    }
+
+    // Set the Header
+    header::type('application/json');
+
+    // Build the response
+    $feed = array(
+        'version' => 'https://jsonfeed.org/version/1',
+        'title' => $options['title'],
+        'home_page_url' => url(),
+        'feed_url' => $options['feed'],
+        'items' => $items
+    );
+
+    return json_encode($feed);
+
+};
